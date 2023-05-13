af05b7ec45a7a169079119231939028be9ba9300
surajk013 <surajsinghk013@gmail.com>
2023-05-13 06:19:19 -0400

    Budget Allocation Application

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,1 +1,17 @@
+import React, {useContext} from 'react';
+import {AppContext} from '../context/AppContext'
+const ExpenseTotal =()=>{
+    const {expenses} = useContext(AppContext);
+    const totalExpenses =expenses.reduce((total,item) =>{
+        return (total += item.cost);
+    },0);
+    return(
+        <div classNAme='alert alert-primary'>
+            <span>Spent so far: £{totalExpenses}</span>
 
+        </div>
+
+    );
+
+};
+export default ExpenseTotal;
